754e199d0ee7f86cf43864d111f5e207596d675d
crypto0627 <jake0627a1@gmail.com>
2022-12-13 17:24:44 +0800

    V2.0

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,23 +1,46 @@
 import React,{ useState,useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
+
+const Web3 = require("web3");
+
 export const Newsletter = () => {
+  
   const [account, setAccount] = useState('');
   //Token address
   let myAddress = "0x13D8CaF1EaBcCBBD00d1E6D2dbB4dc4FECF2a022";
   const transfer = async() =>{
-    if(typeof window!=="undefined"){
-      //Metamask request!
-      const accounts = await window.ethereum.request({
-        method: "eth_accounts",
-      });
-      if (account === accounts[0]) {
-        console.log("web3!!!")
+    try{
+      if(typeof window!=="undefined"){
+        //Metamask request!
+        const accounts = await window.ethereum.request({
+          method: "eth_accounts",
+        });
+        if (account === accounts[0]) {
+          try{
+            const web3 = new Web3('http://localhost:3000/nft-boneboss');
+            const params = {
+              from: account,
+              to: myAddress,
+              value: '1000000000000000',
+              gas:'21000'
+          };
+            await window.ethereum.enable()
+            window.web3 = new Web3(window.ethereum)
+            const sendHash = await window.web3.eth.sendTransaction(params)
+            console.log('txnHash is ' + sendHash)
+          }catch(err){
+            console.log(err.message)
+          }
+        }else{
+          console.log("address isn't your wallet!")
+        }
       }else{
-        console.log("Textbox address isn't connected wallet address!")
+        console.log("Please connect your wallet!")
       }
-    }else{
-      console.log("Please connect your wallet!")
+    }catch(err){
+      console.log(err.message)
     }
+
   }
     useEffect(() => {
       transfer();
