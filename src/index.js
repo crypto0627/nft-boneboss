d7cf05789376aa03a9059da0d1fbe66f4ee42241
jake0627a1 <jake0627a1@gmail.com>
2022-12-16 16:38:39 +0800

    V1.0

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Ampilfy, { Amplify } from "aws-amplify";
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
+;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
